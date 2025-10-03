95f0506c76f78f687d558efe6cc3f414fc636be9
yeahhabebi <yeahhabebi@gmail.com>
2025-10-03 08:00:43 +0600

    Delete render-worker.sh

--- render-worker.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# render-worker.sh
-
-# Install dependencies (optional if Render already does it)
-pip install -r requirements.txt
-
-# Run the fetcher script
-python3 fetcher/fetch_coinryze.py
